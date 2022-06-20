cb59bc61aa28bcf9a3308486e76db2c9dd5e28b4
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-06-21 01:57:26 +0200

    Add phptravels main scenarios test cases

---
+++ src/test/java/phptravels/apis/PhptravelsApis.java
@@ -0,0 +1,46 @@
+package phptravels.apis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import utils.ApiActions;
+import utils.ExtentReport;
+import utils.PropertiesReader;
+import utils.ApiActions.RequestType;
+
+public class PhptravelsApis {
+    String baseUrl = PropertiesReader.getProperty("environment.properties", "phptravels.baseurl");
+    ApiActions api = new ApiActions(baseUrl);
+
+    // Expected status codes
+//    private static final int SUCCESS = 200; 
+    private static final int REDIRECTION = 302;
+    
+    // Services Names
+    private String signup_endpoint = "/signup";
+
+
+    @Step("User Sign up with Data --> First Name: [{firstName}], Last Name: [{lastName}], Mobile Number: [{mobileNumber}], Email: [{email}] and Password: [{password}]")
+    public Response userSignUp(String firstName, String lastName, String mobileNumber, String email, String password, String accountType) {
+	ExtentReport.info(MarkupHelper.createLabel(" Api - User Sign up", ExtentColor.BLUE));
+
+	Map<String, Object> formParams = new HashMap<>();
+	formParams.put("address", "");
+	formParams.put("first_name", firstName);
+	formParams.put("last_name", lastName);
+	formParams.put("phone", mobileNumber);
+	formParams.put("email", email);
+	formParams.put("password", password);
+	formParams.put("type", password);
+
+	return api.performRequest(RequestType.POST, signup_endpoint, REDIRECTION, null, null, formParams, null,
+		null, null);
+
+    }
+
+}
--- src/test/java/phptravels/gui/tests/SignupTests.java
+++ src/test/java/phptravels/gui/tests/SignupTests.java
@@ -3,6 +3,7 @@ package phptravels.gui.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,29 +15,31 @@ import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
-import phptravels.gui.pages.LoginPage;
+import phptravels.gui.pages.SigninPage;
 import phptravels.gui.pages.SignupPage;
+import phptravels.gui.pages.UserAccountLeftMenu;
 import utils.BrowserActions;
 import utils.BrowserFactory;
 import utils.Helper;
 import utils.JsonFileManager;
 
 @Epic("PHPTRAVELS")
-@Feature("Admin User")
+@Feature("User Management")
 public class SignupTests {
     private WebDriver driver;
     
     JsonFileManager testData;
 
     private SignupPage signupPage;
-    private LoginPage loginPage;
+    private SigninPage signinPage;
+    private UserAccountLeftMenu userAccountLeftMenu;
 
     private String currentTime = Helper.getCurrentTime();
 
     //////////////////////////////////////////////////////
     /////////////////// Test Cases //////////////////////
 
-    @Test(description = "PHPTRAVELS - GUI - Valid User Signup")
+    @Test(description = "PHPTRAVELS - Valid User Signup")
     @Description("When I enter valid data in the sign up form And click the signup button, Then I should be registered successfully And be navigated to the user account page And I can see my user data and Hi message")
     @Story("Signup")
     @Severity(SeverityLevel.BLOCKER)
@@ -44,11 +47,14 @@ public class SignupTests {
     public void testingValidUserSignUp() {
 	String email = testData.getTestData("user.email") + currentTime + "@" + testData.getTestData("user.emailDomain");
 	
-	signupPage	.userSignUp(testData.getTestData("user.firstName"), testData.getTestData("user.lastName"), testData.getTestData("user.phoneNumber"), email, testData.getTestData("user.password"), testData.getTestData("user.accountType"));
-	loginPage	.assertOnFormMessage(testData.getTestData("messages.successfulSignup"));
+	signupPage		.userSignUp(testData.getTestData("user.firstName"), testData.getTestData("user.lastName"), testData.getTestData("user.phoneNumber"), email, testData.getTestData("user.password"), testData.getTestData("user.accountType"));
+	signinPage		.assertOnFormMessage(testData.getTestData("messages.successfulSignup"));
+	
+	signinPage		.userLogin(email, testData.getTestData("user.password"));
+	userAccountLeftMenu	.assertOnUserProfileName(testData.getTestData("user.firstName"));
     }
     
-    @Test(description = "PHPTRAVELS - GUI - Invalid User Signup - Email Already Exists", dependsOnMethods = { "testingValidUserSignUp" })
+    @Test(description = "PHPTRAVELS - Invalid User Signup - Email Already Exists", dependsOnMethods = { "testingValidUserSignUp" })
     @Description("Given i already signed up with an email, When I use the same email for new sign up , Then I should get an error message ")
     @Story("Signup")
     @Severity(SeverityLevel.CRITICAL)
@@ -60,7 +66,7 @@ public class SignupTests {
 	signupPage	.assertOnFormMessage(testData.getTestData("messages.emailExists"));
     }
     
-    @Test(description = "PHPTRAVELS - GUI - Invalid User Sign Up - Wrong Email Format")
+    @Test(description = "PHPTRAVELS - Invalid User Sign Up - Wrong Email Format")
     @Description("When I use a wrong email format on the sign up , Then I should get an error message ")
     @Story("Signup")
     @Severity(SeverityLevel.NORMAL)
@@ -73,15 +79,18 @@ public class SignupTests {
 
     //////////////////////////////////////////////////////
     ///////////////// Configurations ////////////////////
+    @BeforeClass
+    public void classSetup() {
+	testData = new JsonFileManager("src/test/resources/TestData/phptravelsTestData.json");
+    }
 
     @BeforeMethod
-    public void methodSetup() {
-	testData = new JsonFileManager("src/test/resources/TestData/phptravelsTestData.json");
-	
+    public void methodSetup() {	
 	driver = BrowserFactory.getBrowser();
 	
 	signupPage = new SignupPage(driver);
-	loginPage = new LoginPage(driver);
+	signinPage = new SigninPage(driver);
+	userAccountLeftMenu = new UserAccountLeftMenu(driver);
 	
 	signupPage.navigateToSignupPage();
     }
